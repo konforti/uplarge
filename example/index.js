f5efc95ea2964a70e7295a35a874ad9c200bd03b
raz <konforti@gmail.com>
2023-08-26 17:23:33 +0300

    Clear File

--- example/index.js
+++ example/index.js
@@ -1,79 +1,104 @@
-import UpLarge from "../src/UpLarge";
+import Uplarge from "../src/Uplarge";
+
+let pickerEl = document.getElementById("picker");
+let clearEl = document.getElementById("clear");
+let statusEl = document.getElementById("status");
+let progressEl = document.getElementById("progress");
+let urlEl = document.getElementById("url");
+let errEl = document.getElementById("err");
 
 function upload(file) {
-  let props = {
-    cloudName: "dxuiuruim",
-    uploadPreset: "unsigned"
-  };
-  let upLarge = UpLarge(props);
-  upLarge.uploadFile(file);
+    let props = {
+        cloudName: "demo",
+        uploadPreset: "unsigned",
+    };
+    let uplarge = Uplarge(props);
+    uplarge.uploadFile(file);
 
-  upLarge.emitter.addEventListener("progress", (evt) => {
-    document.getElementById("progress").value = Math.round(evt.detail.percent);
-    document.getElementById("status").innerHTML = `${Math.round(
-      evt.detail.percent
-    )}%`;
-  });
-  upLarge.emitter.addEventListener("success", (evt) => {
-    let res = evt.detail.response;
-    let url = res.playback_url;
-    if (res.resource_type !== "video") {
-      url = res.secure_url;
-    }
-    document.getElementById("url").innerHTML = url;
-    document.getElementById("url").href = url;
-  });
-  upLarge.emitter.addEventListener("error", (evt) => {
-    document.getElementById(
-      "status"
-    ).innerHTML = `Error during the upload: [${evt.detail.status}] ${evt.detail.message}.`;
-  });
+    uplarge.on.addEventListener("progress", (evt) => {
+        progressEl.value = Math.round(evt.detail.percent);
+        statusEl.innerHTML = `${Math.round(evt.detail.percent)}%`;
+    });
+    uplarge.on.addEventListener("success", (evt) => {
+        let res = evt.detail.response;
+        let url = res.playback_url;
+        if (res.resource_type !== "video") {
+            url = res.secure_url;
+        }
+        urlEl.innerHTML = url;
+        urlEl.href = url;
+    });
+    uplarge.on.addEventListener("error", (evt) => {
+        errEl.innerHTML = `Error during the upload: [${evt.detail.status}] ${evt.detail.message}.`;
+    });
 }
 
-let picker = document.getElementById("picker");
-picker.addEventListener("change", (e) => {
-  upload(picker.files[0]);
+function pickerAction() {
+    if (pickerEl.value) {
+        pickerEl.disabled = "disabled";
+    } else {
+        pickerEl.disabled = null;
+        progressEl.value = 0;
+        statusEl.innerHTML = "0%";
+        urlEl.innerHTML = null;
+        urlEl.href = null;
+    }
+}
+
+clearEl.addEventListener("click", (e) => {
+    if (!pickerEl.value) return;
+    pickerEl.value = null;
+    pickerAction();
+});
+
+pickerEl.addEventListener("input", (e) => {
+    upload(pickerEl.files[0]);
+    pickerAction();
 });
 
 let container = document.querySelector(".container");
 container.addEventListener(
-  "dragenter",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.add("active");
-  },
-  false
+    "dragenter",
+    (e) => {
+        if (pickerEl.value) return;
+        e.preventDefault();
+        e.stopPropagation();
+        container.classList.add("active");
+    },
+    false
 );
 
 container.addEventListener(
-  "dragleave",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.remove("active");
-  },
-  false
+    "dragleave",
+    (e) => {
+        if (pickerEl.value) return;
+        e.preventDefault();
+        e.stopPropagation();
+        container.classList.remove("active");
+    },
+    false
 );
 
 container.addEventListener(
-  "dragover",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.add("active");
-  },
-  false
+    "dragover",
+    (e) => {
+        if (pickerEl.value) return;
+        e.preventDefault();
+        e.stopPropagation();
+        container.classList.add("active");
+    },
+    false
 );
 
 container.addEventListener(
-  "drop",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.remove("active");
-    let draggedData = e.dataTransfer;
-    upload(draggedData.files[0]);
-  },
-  false
+    "drop",
+    (e) => {
+        if (pickerEl.value) return;
+        e.preventDefault();
+        e.stopPropagation();
+        container.classList.remove("active");
+        let draggedData = e.dataTransfer;
+        upload(draggedData.files[0]);
+    },
+    false
 );
--- src/UpLarge.js
+++ src/UpLarge.js
@@ -1,153 +1,158 @@
 let cloudName,
-  publicId,
-  uploadPreset,
-  chunkSize,
-  xUniqueUploadId,
-  chunkCount,
-  totalChunks,
-  file,
-  fileSize,
-  fromByte,
-  toByte,
-  retries,
-  retriesCount,
-  eventTarget;
+    publicId,
+    uploadPreset,
+    chunkSize,
+    xUniqueUploadId,
+    chunkCount,
+    totalChunks,
+    file,
+    fileSize,
+    fromByte,
+    toByte,
+    retries,
+    retriesCount,
+    eventTarget;
 
-function UpLarge(props) {
-  xUniqueUploadId = +new Date();
-  publicId = (props && props.publicId) || xUniqueUploadId;
-  cloudName = (props && props.cloudName) || "demo";
-  uploadPreset = (props && props.uploadPreset) || "unsigned";
-  chunkSize = 6000000; // Bytes (must be larger than 5,000,000)
-  fromByte = 0;
-  toByte = 0;
-  chunkCount = 0;
-  retries = 3;
-  retriesCount = 0;
-  eventTarget = new EventTarget();
-  return {
-    emitter: eventTarget,
-    uploadFile: uploadFile
-  };
+function Uplarge(props) {
+    xUniqueUploadId = +new Date();
+    publicId = (props && props.publicId) || xUniqueUploadId;
+    cloudName = (props && props.cloudName) || "demo";
+    uploadPreset = (props && props.uploadPreset) || "unsigned";
+    chunkSize = 6000000; // Bytes (must be larger than 5,000,000)
+    fromByte = 0;
+    toByte = 0;
+    chunkCount = 0;
+    retries = 3;
+    retriesCount = 0;
+    eventTarget = new EventTarget();
+    return {
+        on: eventTarget,
+        uploadFile: uploadFile,
+    };
 }
 
 function uploadFile(fileToUpload) {
-  if (!fileToUpload) {
-    return new TypeError("The file is missing");
-  }
-  file = fileToUpload;
-  fileSize = file.size;
-  totalChunks = Math.ceil(fileSize / chunkSize);
-  processFile();
+    if (!fileToUpload) {
+        return new TypeError("The file is missing");
+    }
+    file = fileToUpload;
+    fileSize = file.size;
+    totalChunks = Math.ceil(fileSize / chunkSize);
+    processFile();
 }
 
 function processFile(retry = false) {
-  toByte = fromByte + parseInt(chunkSize);
-  if (toByte > fileSize) {
-    toByte = fileSize;
-  }
-  let part = file.slice(fromByte, toByte);
-  if (!retry) {
-    chunkCount++;
-  }
-  send(part, fromByte, toByte - 1, fileSize)
-    .then((res) => {
-      if (parseInt(res.status / 100) === 2) {
-        retriesCount = 0;
-        if (toByte < fileSize) {
-          fromByte = toByte;
-          processFile();
-        } else {
-          eventTarget.dispatchEvent(
-            new CustomEvent("success", {
-              detail: { response: JSON.parse(res.response) }
-            })
-          );
-        }
-      } else if (retriesCount++ <= retries) {
-        processFile(true);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  function send(part, fromByte, toByte, fileSize) {
-    let formdata = new FormData();
-    formdata.append("file", part);
-    formdata.append("upload_preset", uploadPreset);
-    formdata.append("cloud_name", cloudName);
-    formdata.append("public_id", publicId);
-
-    let xhr = new XMLHttpRequest();
-
-    return new Promise(function (resolve, reject) {
-      xhr.onload = () => {
-        // Fired when an XMLHttpRequest transaction completes successfully
-        if (xhr.readyState !== 4) {
-          return; // Only run if the request is complete (4 == Done)
-        }
-        if (parseInt(xhr.status / 100) === 2) {
-          resolve(xhr);
-        } else {
-          reject({
-            status: xhr.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-
-      xhr.open(
-        "POST",
-        `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`
-      );
-      xhr.timeout = 60 * 60 * 1000;
-      xhr.setRequestHeader("X-Unique-Upload-Id", xUniqueUploadId);
-      xhr.setRequestHeader(
-        "Content-Range",
-        `bytes ${fromByte}-${toByte}/${fileSize}`
-      );
-      xhr.upload.onloadstart = function () {
-        // Fired when a request has started to load data
-      };
-
-      xhr.onloadend = function () {
-        // Fired when a request has been completed, whether successfully (after load) or unsuccessfully (after abort or error)
-      };
-
-      xhr.upload.onabort = function () {
-        // Fired when a request has been aborted, for example, because the program called XMLHttpRequest.abort().
-      };
-
-      xhr.upload.onprogress = function (event) {
-        // Fired periodically when a request receives more data
-        let total = Math.max(event.total, fileSize);
-        let uploaded = (chunkCount - 1) * chunkSize + event.loaded;
-        let percent = (uploaded / total) * 100;
-        eventTarget.dispatchEvent(
-          new CustomEvent("progress", { detail: { percent: percent } })
-        );
-      };
-
-      xhr.upload.ontimeout = function () {
-        // Fired when progress is terminated due to preset time expiring
-      };
-
-      xhr.upload.onerror = function (event) {
-        eventTarget.dispatchEvent(
-          new CustomEvent("error", {
-            detail: { status: xhr.status, message: event.target.responseText }
-          })
-        );
-        reject({
-          status: xhr.status,
-          statusText: xhr.statusText
+    toByte = fromByte + parseInt(chunkSize);
+    if (toByte > fileSize) {
+        toByte = fileSize;
+    }
+    let part = file.slice(fromByte, toByte);
+    if (!retry) {
+        chunkCount++;
+    }
+    send(part, fromByte, toByte - 1, fileSize)
+        .then((res) => {
+            if (parseInt(res.status / 100) === 2) {
+                retriesCount = 0;
+                if (toByte < fileSize) {
+                    fromByte = toByte;
+                    processFile();
+                } else {
+                    eventTarget.dispatchEvent(
+                        new CustomEvent("success", {
+                            detail: { response: JSON.parse(res.response) },
+                        })
+                    );
+                }
+            } else if (retriesCount++ <= retries) {
+                processFile(true);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
         });
-      };
 
-      xhr.send(formdata);
-    });
-  }
+    function send(part, fromByte, toByte, fileSize) {
+        let formdata = new FormData();
+        formdata.append("file", part);
+        formdata.append("upload_preset", uploadPreset);
+        formdata.append("cloud_name", cloudName);
+        formdata.append("public_id", publicId);
+
+        let xhr = new XMLHttpRequest();
+
+        return new Promise(function (resolve, reject) {
+            xhr.onload = () => {
+                // Fired when an XMLHttpRequest transaction completes successfully
+                if (xhr.readyState !== 4) {
+                    return; // Only run if the request is complete (4 == Done)
+                }
+                if (parseInt(xhr.status / 100) === 2) {
+                    resolve(xhr);
+                } else {
+                    reject({
+                        status: xhr.status,
+                        statusText: xhr.statusText,
+                    });
+                }
+            };
+
+            xhr.open(
+                "POST",
+                `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`
+            );
+            xhr.timeout = 60 * 60 * 1000;
+            xhr.setRequestHeader("X-Unique-Upload-Id", xUniqueUploadId);
+            xhr.setRequestHeader(
+                "Content-Range",
+                `bytes ${fromByte}-${toByte}/${fileSize}`
+            );
+            xhr.upload.onloadstart = function () {
+                // Fired when a request has started to load data
+            };
+
+            xhr.onloadend = function () {
+                // Fired when a request has been completed, whether successfully (after load) or unsuccessfully (after abort or error)
+            };
+
+            xhr.upload.onabort = function () {
+                // Fired when a request has been aborted, for example, because the program called XMLHttpRequest.abort().
+            };
+
+            xhr.upload.onprogress = function (event) {
+                // Fired periodically when a request receives more data
+                let total = Math.max(event.total, fileSize);
+                let uploaded = (chunkCount - 1) * chunkSize + event.loaded;
+                let percent = (uploaded / total) * 100;
+                eventTarget.dispatchEvent(
+                    new CustomEvent("progress", {
+                        detail: { percent: percent },
+                    })
+                );
+            };
+
+            xhr.upload.ontimeout = function () {
+                // Fired when progress is terminated due to preset time expiring
+            };
+
+            xhr.upload.onerror = function (event) {
+                eventTarget.dispatchEvent(
+                    new CustomEvent("error", {
+                        detail: {
+                            status: xhr.status,
+                            message: event.target.responseText,
+                        },
+                    })
+                );
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText,
+                });
+            };
+
+            xhr.send(formdata);
+        });
+    }
 }
 
-export default UpLarge;
+export default Uplarge;
